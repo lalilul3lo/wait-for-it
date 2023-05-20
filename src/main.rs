d82a956d3e9c754c3462f162dbe4ee325f0c64a6
Samuel Joli <samuel.joli.ftn@gmail.com>
2023-05-20 02:25:19 -0400

    Initial implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,74 @@
+use clap::{Arg, ArgAction, Command};
+use std::net::TcpStream;
+use std::process::exit;
+use std::thread::sleep;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let cmd = Command::new(clap::crate_name!())
+        .version(clap::crate_version!())
+        .author(clap::crate_authors!("\n"))
+        .about(clap::crate_description!())
+        .arg(Arg::new("path").action(ArgAction::Append))
+        .arg(
+            Arg::new("host")
+                .long("host")
+                .required(false)
+                .help("Host or IP being probed."),
+        )
+        .arg(
+            Arg::new("port")
+                .long("port")
+                .short('p')
+                .required(false)
+                .help("TCP port being probed."),
+        )
+        .arg(
+            Arg::new("timeout")
+                .long("timeout")
+                .short('t')
+                .required(false)
+                .default_value("10")
+                .help("Timeout in seconds before quitting."),
+        );
+
+    let matches = cmd.get_matches();
+
+    let host = matches.get_one::<String>("host").unwrap();
+
+    let port = matches.get_one::<String>("port").unwrap();
+
+    let timeout: u64 = matches
+        .get_one::<String>("timeout")
+        .unwrap()
+        .parse::<u64>()
+        .unwrap();
+
+    let timeout = Duration::from_secs(timeout);
+
+    let start = std::time::Instant::now();
+
+    loop {
+        if start.elapsed() > timeout {
+            println!("process timed out after {:?}s", timeout.as_secs());
+            exit(0)
+        }
+
+        match TcpStream::connect(format!("{}:{}", host, port)) {
+            Ok(_) => {
+                println!(
+                    "{}:{} is available after {} seconds",
+                    host,
+                    port,
+                    start.elapsed().as_secs()
+                );
+
+                exit(0);
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                sleep(Duration::from_secs(1));
+            }
+        }
+    }
 }
---
+++ tests/lib.rs
@@ -0,0 +1,27 @@
+use assert_cmd::Command;
+use std::thread;
+use std::time::Duration;
+use tokio::net::TcpListener;
+
+#[tokio::test]
+async fn test_host_port_as_flags() {
+    // Start a TCP server
+    let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
+    let addr = listener.local_addr().unwrap();
+
+    // Spawn a task to accept the connection
+    tokio::spawn(async move {
+        listener.accept().await.unwrap();
+    });
+
+    // Run the CLI tool to wait for the TCP server
+    thread::sleep(Duration::from_secs(1)); // wait for the server to start
+    let mut cmd = Command::cargo_bin("wait_for_it").unwrap();
+    cmd.arg("--host")
+        .arg(addr.ip().to_string())
+        .arg("--port")
+        .arg(addr.port().to_string())
+        .arg("--timeout")
+        .arg("5");
+    cmd.assert().success();
+}
